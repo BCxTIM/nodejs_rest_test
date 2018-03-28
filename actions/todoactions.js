3c4a5106898437836a636e80413efdc4c83a4a94
tmoiseev <verront@gmail.com>
2018-03-28 18:09:21 +0300

    Refactor logic of scenarios

--- actions/todoactions.js
+++ actions/todoactions.js
@@ -6,85 +6,62 @@ const url = "http://localhost:5000";
 var api = supertest(url);
 var should = require('should');
 
+
 /**
- *
- * @param message
- * @HTTP status - HTTP response code
- * @boolean
+ * Get all tasks
+ * @HTTP status
+ * @boolean error
  */
-exports.getAllTasks = function (message, status, error) {
-    it(message, function (done) {
-        api
-            .get('/todos')
-            .end(function (err, res) {
-                res.statusCode.should.equal(status);
-                res.body.error.should.equal(error);
-                done();
-            });
-    });
+exports.getAllTasks = function (status, error) {
+    api
+        .get('/todos')
+        .end(function (err, res) {
+            res.statusCode.should.equal(status);
+            res.body.error.should.equal(error);
+        });
 };
 
 /**
  *
- * @param message
- * @HTTP id - task ID
- * @param status - HTTP response code
- * @boolean error - boolean
+ * @param id
+ * @HTTP status
+ * @boolean error
  */
 
-exports.getTaskById = function (message, id, status, error) {
-    it(message, function (done) {
-        api
-            .get('/todo/' + id)
-            .end(function (err, res) {
-                res.statusCode.should.equal(status);
-                res.body.error.should.equal(error);
-                done();
-            });
-    });
+exports.getTaskById = function (id, status, error) {
+    api
+        .get('/todo/' + id)
+        .end(function (err, res) {
+            res.statusCode.should.equal(status);
+            res.body.error.should.equal(error);
+            if (res.body.error) {
+                res.body.data.should.equal("Task with id " + id + " not found");
+            }
+        });
 };
 
 /**
  *
- * @param message
- * @HTTP status - HTTP response code
+ * @HTTP status
  * @boolean error
  * @JSON todo
+ * @param callback
  */
 
-exports.createTodo = function (message, status, error, todo) {
-    it(message, function (done) {
-        const resolving = new Promise(function (resolve) {
-            api
-                .post('/todo')
-                .send(todo)
-                .end(function (err, res) {
-                    res.statusCode.should.equal(status);
-                    res.body.error.should.equal(error);
-                    if(error) {
-                        res.body.data.should.equal("Task could not be empty");
-                    } else {
-                        res.body.data.task.should.equal(todo.task);
-                    }
-                    resolve(res.body);
-                });
+exports.createTodo = function (status, error, todo, callback) {
+    api
+        .post('/todo')
+        .send(todo)
+        .end(function (err, res) {
+            res.statusCode.should.equal(status);
+            res.body.error.should.equal(error);
+            if (error) {
+                res.body.data.should.equal("Task could not be empty");
+            } else {
+                res.body.data.task.should.equal(todo.task);
+            }
+            callback(res.body);
         });
-        resolving.then(function (result) {
-            console.log(result.data);
-            api
-                .get('/todo/' + result.data.id)
-                .end(function (err, res) {
-                    res.statusCode.should.equal(status);
-                    res.body.error.should.equal(error);
-                    if (error) {
-                        res.body.data.should.equal("Task with id " + result.id + " not found");
-                    } else {
-                        res.body.data.task.should.equal(result.data.task);
-                    }
-                    done();
-                })
-        })
-    });
 };
 
 
--- test/todos.test.js
+++ test/todos.test.js
@@ -6,33 +6,65 @@ var Task = require('../models/todo');
 
 describe("Get todos", function () {
     describe('#GET / todos', function () {
-        todoactions.getAllTasks("should get task by id", 200, false);
+        it("Should get all todos", function (done) {
+            todoactions.getAllTasks(200, false);
+            done();
+        })
     });
 
     describe('#GET / todo by existing id', function () {
-        todoactions.getTaskById("should get task by id", 1, 200, false);
+        it("Should get todo by id", function (done) {
+            todoactions.getTaskById(1, 200, false);
+            done();
+        })
     });
 
     describe('#GET / todo by not existing id', function () {
-        todoactions.getTaskById("should get task by id", 666, 400, true);
+        it("should not get task by not existing id", function (done) {
+            todoactions.getTaskById(666, 400, true);
+            done();
+        });
     });
 
     describe('#GET / todo by not incorrect id', function () {
-        todoactions.getTaskById("should get task by id", "dsadsad", 400, true);
+        it("should not get task by incorrect id", function (done) {
+            todoactions.getTaskById("dsadsad", 400, true);
+            done();
+        });
     });
-
 });
 
 
-describe("Create todo and verify that was created", function () {
+describe("Testing creation todo", function () {
     describe("Create valid todo", function () {
-            var task = new Task("Tim todo");
-            todoactions.createTodo("Create new todo", 200, false, task);
+        let task = new Task("Tim todo");
+        it("Should create a new todo and verify if is created", function (done) {
+            let promise = new Promise(function (resolve) {
+                todoactions.createTodo(200, false, task, function (response) {
+                    resolve(response);
+                });
+            });
+            promise.then(function (result) {
+                todoactions.getTaskById(result.data.id, 200, false);
+                done();
+            })
         });
 
+    });
+
     describe("Do not create todo without task", function () {
-        var task = new Task("");
-        todoactions.createTodo("Create new todo", 400, true, task);
+        let task = new Task("");
+        it("Should not create a new todo and verify if is not created", function (done) {
+            let promise = new Promise(function (resolve) {
+                todoactions.createTodo(400, true, task, function (response) {
+                    resolve(response);
+                });
+            });
+            promise.then(function (result) {
+                todoactions.getTaskById(result.data.id, 400, true);
+                done();
+            })
+        });
     });
 });
 
